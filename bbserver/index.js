e4fb12cc5357f9087ddb9c435b57451855c3125a
raju0005 <anderajesh15@gmail.com>
2025-07-18 22:39:39 +0530

    bug : cant find the error

--- bbserver/index.js
+++ bbserver/index.js
@@ -1,32 +1,43 @@
+//PATHS
+import path from "path";
 import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
 import dotenv from "dotenv";
-import UserRoutes from "../../routes/UserRoutes.js";
-import donorRoutes from "../../routes/donorRoutes.js";
-import connectDB from "../../config/db.js";
+import cookieParser from "cookie-parser";
+//utils
+import connectDB from "./config/db.js";
+//routes
+import UserRoutes from "./routes/UserRoutes.js";
+import donorRoutes from "./routes/donorRoutes.js";
 
 dotenv.config();
-
-const app = express();
+const port = process.env.PORT || 5000;
 
 connectDB();
 
+const app = express();
+
+import cors from 'cors';
 app.use(cors({
-  origin: 'https://blood-bridge-app.vercel.app',
-  credentials: true,
+  origin: 'https://blood-bridge-app.vercel.app', // Or use "*" if public API
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // if you're sending cookies
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send("Welcome to BB Server ✅");
+  res.send("Welcome to BB Server ❌");
 });
 
 app.use("/api/bb/user", UserRoutes);
 app.use("/api/bb/donors", donorRoutes);
 
-// Vercel-style export
-export default app;
+// app.listen(port, () => {
+//   console.log(`Server Connected Successfully 🚀 running on ${port}`);
+// });
+export default function handler(req, res) {
+  app(req, res);
+}
