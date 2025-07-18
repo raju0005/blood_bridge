3d011e9dd972c04aeff1d8a0786eb35babd7bd71
raju0005 <anderajesh15@gmail.com>
2025-07-18 11:19:08 +0530

    feat: add vercel.json file for deployment of backend

--- bbserver/index.js
+++ bbserver/index.js
@@ -1,32 +1,33 @@
 //PATHS
-import path from 'path'
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 //utils
-import connectDB from './config/db.js'
+import connectDB from "./config/db.js";
 //routes
-import UserRoutes from './routes/UserRoutes.js'
-import donorRoutes from './routes/donorRoutes.js'
+import UserRoutes from "./routes/UserRoutes.js";
+import donorRoutes from "./routes/donorRoutes.js";
 
-dotenv.config()
-const port= process.env.PORT || 5000
+dotenv.config();
+const port = process.env.PORT || 5000;
 
-connectDB()
+connectDB();
 
-const app=express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use('/api/bb/user',UserRoutes)
-app.use('/api/bb/donors',donorRoutes)
+app.use("/", (req, res) => {
+  res.send("Welcome to BB Server ❌");
+});
 
+app.use("/api/bb/user", UserRoutes);
+app.use("/api/bb/donors", donorRoutes);
 
-app.listen(port , ()=>{
-    console.log(`Server Connected Successfully 🚀 running on ${port}`)
-})
-export default function handler(req, res) {
-    app(req, res);
-  }
+// app.listen(port, () => {
+//   console.log(`Server Connected Successfully 🚀 running on ${port}`);
+// });
+export default app;
